5c9339ed0d8fc2743f1f8dad3961ee753e3fa296
Abha-1281 <61627513+Abha-1281@users.noreply.github.com>
2024-11-16 20:20:01 +0530

    Added comment

--- src/Array/MoveNegativeElementsToOneSide.java
+++ src/Array/MoveNegativeElementsToOneSide.java
@@ -18,6 +18,7 @@ public class MoveNegativeElementsToOneSide {
     public static void main(String[] args) {
         int[] numbers = {-1,2,-3,4,5,6,-7,8,9};
         moveNegativeElements(numbers);
+        // print the output
         System.out.println(Arrays.toString(numbers));
     }
 }
