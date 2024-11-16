16aa6eddd7ab6853ff730467b0334b19d357045f
Abha-1281 <61627513+Abha-1281@users.noreply.github.com>
2024-11-16 20:05:21 +0530

    Removed comment

--- src/Array/MoveNegativeElementsToOneSide.java
+++ src/Array/MoveNegativeElementsToOneSide.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class MoveNegativeElementsToOneSide {
 
     public static void moveNegativeElements(int[] arr){
-        int j = 0; // to keep the track of storing the negative numbers
+        int j = 0;
         for(int i = 0; i < arr.length; i++){
             if(arr[i]<0){
                 int temp = arr[i];
